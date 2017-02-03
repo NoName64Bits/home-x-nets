9364a605fe47b83ec7359d50fb10932274d35627
NoName64Bits <ciobanulaur@gmail.com>
2017-02-03 20:29:54 +0200

    blahhh

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res){
-  res.send("Welcome!");
-});
-
 app.get('/:title', function(req, res){
   res.send("<head><title>" + req.params.title + "</title></head>Okkk");
 });
